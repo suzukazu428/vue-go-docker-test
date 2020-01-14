bf1df46a72b3995335cf6e4467c02654ef2f2fe3
鈴木　和弥 <>
2020-01-14 10:21:56 +0900

    first commit

---
+++ assets/README.md
@@ -0,0 +1,5 @@
+## Project setup
+```
+yarn install
+docker-compose up
+```
---
+++ assets/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  async created () {
+    try {
+      // get()の中のURLは、nginx.confで設定してるので、/api/となっている
+      const res = await this.$axios.get('http://localhost/api/')
+      console.log(res)
+    } catch(e) {
+      console.log('error', e)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
